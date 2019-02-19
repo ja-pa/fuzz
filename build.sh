e4116ecd3c4cf16d37223c79eb65b7b74838778e
Jan Pavlinec <jan.pavlinec@nic.cz>
2019-02-19 10:23:34 +0100

    build.sh add functions

--- build.sh
+++ build.sh
@@ -1,12 +1,43 @@
 #!/bin/bash
 
-PATH_AFL=/home/....../afl-2.52b
+PATH_AFL=/home/cznic/data/src/fuzz_test/afl-2.52b
+
 SANITIZER_VARS=	-DCMAKE_CXX_FLAGS="-fsanitize=address  -fsanitize=leak -g" \
-		-DCMAKE_C_FLAGS="-fsanitize=address  -fsanitize=leak -g" \
+		-DCMAKE_C_FLAGS="-fsanitize=address  -fsanitize=leak -g -fsanitize-recover=address" \
 		-DCMAKE_EXE_LINKER_FLAGS="-fsanitize=address  -fsanitize=leak" \
 		-DCMAKE_MODULE_LINKER_FLAGS="-fsanitize=address  -fsanitize=leak"
 
 
+compile_pkg() {
 
-cmake -DCMAKE_C_COMPILER=$PATH_AFL/afl-gcc $SANITIZER_VARS .
+	if [ -z "$1" ]; then
+		cmake -DCMAKE_C_COMPILER=$PATH_AFL/afl-gcc .
+	else
+		cmake -DCMAKE_C_COMPILER=$PATH_AFL/afl-gcc $SANITIZER_VARS .
+	fi
+}
 
+clean_pkg() {
+	find . -name CMakeFiles
+	find . -name CMakeCache.txt
+}
+
+
+print_help() {
+	echo "Help:"
+}
+
+case "$1" in
+"compile")
+	compile_pkg
+    	;;
+"compile_asan")
+	compile_pkg "asan"
+    	;;
+"clean")
+	clean_pkg
+	;;
+*)
+	print_help
+    ;;
+esac
